2b44e1fb7332db52280b2fdf753b19ce41af5925
Alalexb <baranovaleksej514@gmail.com>
2024-07-26 07:11:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const staticCacheName = 'CacheSpace1';
+const assetUrls = [
+  '/icons',
+  '/css/style.css',
+  '/background',
+  '/index.php',
+  '/фон.jpg',
+  '/бургер.jpg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(staticCacheName);
+      for (const url of assetUrls) {
+        try {
+          await cache.add(url);
+          console.log(Cached asset: ${url});
+        } catch (error) {
+          console.error(Failed to cache asset: ${url}, error);
+          throw error;  
+        }
+      }
+    })()
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[SW]: activate');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch', event.request.url);
+  event.respondWith(cacheFirst(event.request));
+});
+
+async function cacheFirst(request) {
+  const cache = await caches.open(staticCacheName);
+  const cached = await cache.match(request);
+  return cached ?? await fetch(request);
+}
